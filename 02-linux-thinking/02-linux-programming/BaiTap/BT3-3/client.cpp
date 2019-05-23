13a42d7fe02d0ab0a07d7d9b5cd706194bdd1482
1653072@student.hcmus.edu.vn <1653072@student.hcmus.edu.vn>
2019-05-23 19:27:22 +0700

    update

---
+++ 02-linux-thinking/02-linux-programming/BaiTap/BT3-3/client.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <vector>
+#include <netdb.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h> 
+#include <sys/types.h> 
+
+#define PORT 8080
+#define SA struct sockaddr
+
+//====================================================================
+//DECLARE FUNCTIONS
+void run(int sock, vector<int> &marbleArray);
+void func_test(int sock);
+
+
+//====================================================================
+//IMPLEMENT FUNCTIONS
+void run(int sock, vector<int> &marbleArray) {
+	int check;
+	char buffer[256];
+	
+	/* Read message from server */
+	// bzero(buffer, 256);
+	// check = read(sock, buffer, 255);
+	// if (check < 0) {
+	// 	printf(">>ERROR on reading from socket!\n");
+	// 	exit(0);
+	// }
+	// printf("[FROM SERVER] %s\n", buffer);
+
+	while(1) {
+		write(sock, "RequestMarble", 13);
+
+		int val;
+		read(sock, &val, sizeof(val));
+		val=ntohl(val);
+		if (val==-1) break;
+		else
+		{
+			cout<<"[FROM SERVER][Client 1] Received: "<<val<<endl;
+			marbleArray.push_back(val);
+		}
+	}
+
+	cout<<"[Cli 1]"<<marbleArray.size()<<endl;
+
+	
+	/* Get the number of marbles */
+	// int marbleArrSize = -1;
+	// read(sock, &marbleArrSize, sizeof(marbleArrSize));
+	// marbleArrSize=ntohl(marbleArrSize);
+	// printf("[FROM SERVER] Quantity of marbles: %d\n", marbleArrSize);
+	
+	// /* Request marble if server still has marbles */
+	// if (marbleArrSize != -1) {
+	// 	if (marbleArrSize!=0) {
+	// 		write(sock, "RequestMarble", 13);
+	// 	}
+	// 	else break;	/* marbleArrSize == 0: Out of stock */
+	// }
+
+	// /* Get out of stock from server */
+	// bzero(buffer, 256);
+	// read(sock, buffer, 255);
+	// if (strcmp(buffer,"OutOfStock") == 0) break;
+	// else {
+	// 	int val;
+	// 	read(sock, &val, sizeof(val));
+	// 	val=ntohl(val);
+	// 	cout<<"[FROM SERVER] Received: "<<val<<endl;
+	// 	marbleArray.push_back(val);
+	// }
+}
+
+void func_test(int sock) {
+	int check;
+	char buffer[256];
+	
+	while (1)
+	{
+		/* Send message to server */
+		printf("Please enter the message: ");
+		bzero(buffer, 256);
+		fgets(buffer, 255, stdin);
+		check = write(sock, buffer, strlen(buffer));
+		if (check < 0) {
+			printf(">>ERROR on writing to socket!\n");
+			exit(0);
+		}
+
+		/* Read message from server */
+		bzero(buffer, 256);
+		check = read(sock, buffer, 255);
+		if (check < 0) {
+			printf(">>ERROR on reading from socket!\n");
+			exit(0);
+		}
+		printf(">>The message from server: %s\n", buffer);
+	}
+}
+
+
+//====================================================================
+//MAIN FUNCTIONS
+int main() {
+    int sockfd;
+    struct sockaddr_in servaddr; 
+	vector<int> marbleArray;
+
+	/* First call to socket() function to create */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		printf(">>ERROR on opening socket!\n");
+		exit(0);
+	}
+
+	/* Initialize socket structure (assign IP, PORT) */
+	bzero((char *)&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(PORT);
+
+	/* Now connect the client socket to the server socket */
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
+		printf(">>ERROR on connecting to server socket!\n");
+		exit(0);
+	}
+
+	run(sockfd, marbleArray);
+    
+    close(sockfd); 
+	return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+/*#define MAX 80
+void func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	for (;;) {
+		bzero(buff, sizeof(buff));
+		printf("Enter the string : ");
+		n = 0;
+		while ((buff[n++] = getchar()) != '\n')
+			;
+		write(sockfd, buff, sizeof(buff));
+		bzero(buff, sizeof(buff));
+		read(sockfd, buff, sizeof(buff));
+		printf("From Server : %s", buff);
+		if ((strncmp(buff, "exit", 4)) == 0) {
+			printf("Client Exit...\n");
+			break;
+		}
+	}
+}*/
+
---
+++ 02-linux-thinking/02-linux-programming/BaiTap/BT3-3/server.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <vector>
+#include <netdb.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <pthread.h>
+
+#define PORT 8080 
+#define SA struct sockaddr 
+
+//====================================================================
+//GLOBAL VARIABLES
+vector<int> marbleArray, dupMarbleArray;
+pthread_mutex_t lock;
+
+
+//====================================================================
+//DECLARE FUNCTIONS
+vector<int> randomArray();
+void *run(void *argsock);
+void func_test(int sock);
+
+
+//====================================================================
+//IMPLEMENT FUNCTIONS
+vector<int> randomArray() {
+	srand(time(NULL));
+	int arrsize = rand()%898 + 101; //Value belongs to [101, 999]
+	vector<int> newarr;
+
+	for (int i=0; i<arrsize; ++i) {
+		newarr.push_back(rand() % 1000); //The maximum value received from randomization is 1000
+	}
+
+	return newarr;
+}
+
+void *run(void *argsock) {
+	/* Get the socket descriptor */
+	int sock = *((int *) argsock);
+	char buffer[256];
+
+	while(1) {
+		/* Receive "RequestMarble" from client */
+		bzero(buffer, 256);
+		read(sock, buffer, 255);
+		if (strcmp(buffer,"RequestMarble") == 0) 
+		{
+			/* Send first value of marble array to client */
+			pthread_mutex_lock(&lock);
+			if (dupMarbleArray.size() != 0) 
+			{
+				int val = htonl(dupMarbleArray[0]);
+				dupMarbleArray.erase(dupMarbleArray.begin());
+				write(sock, &val, sizeof(val));
+			}
+			/* Send -1 (out of stock) */
+			else 
+			{
+				int val = htonl(-1);
+				write(sock, &val, sizeof(val));
+			}
+			pthread_mutex_unlock(&lock);
+		}
+	}
+
+
+	/* Announce quantity of marbles */
+	// int var = htonl(dupMarbleArray.size());
+	// write(sock, &var, sizeof(var));
+
+	// /* Receive "RequestMarble" from client */
+	// read(sock, buffer, 255);
+	// if (strcmp(buffer,"RequestMarble") == 0) 
+	// {
+	// 	/* Send first value of marble array to client */
+	// 	if (dupMarbleArray.size() != 0) 
+	// 	{
+	// 		int val = htonl(dupMarbleArray[0]);
+	// 		dupMarbleArray.erase(dupMarbleArray.begin());
+	// 		write(sock, &val, sizeof(val));
+	// 	}
+	// 	/* Send out of stock */
+	// 	else 
+	// 	{
+	// 		write(sock, "OutOfStock", 10);
+	// 	}
+	// }
+
+	/* Close socket & thread */
+    close(sock);
+    pthread_exit(NULL); 
+}
+
+void func_test(int sock) {
+	int check;
+	char buffer[256];
+	bzero(buffer, 256);
+
+	while (1)
+	{
+		/* Read message from client */
+		check = read(sock, buffer, 255);
+		if (check < 0) {
+			printf(">>ERROR on reading from socket!\n");
+			exit(0);
+		}
+		printf(">>Here is the message: %s\n", buffer);
+
+		/* Send message to client */
+		check = write(sock, "I got your message!", 19);
+		if (check < 0) {
+			printf(">>ERROR on writing to socket!\n");
+			exit(0);
+		}
+	}
+}
+
+
+//====================================================================
+//MAIN FUNCTIONS
+int main() {
+    int sockfd;
+	vector<int> newsockfd;
+	socklen_t clilen;
+    struct sockaddr_in servaddr, cliaddr;  
+	marbleArray = randomArray();
+	dupMarbleArray = marbleArray;
+	int countclient = 0;
+
+	/* Input the quantity of client */
+	while (countclient<3 || countclient>9) {
+		printf("Please input the quantity of client (>2 and <10): ");
+		scanf("%d", &countclient);
+	}
+	
+	//test
+	for (int i=0; i<dupMarbleArray.size(); ++i) cout<<dupMarbleArray[i]<<endl;
+	cout<<"SIZE: "<<dupMarbleArray.size()<<endl;
+
+	/* Initial mutex */
+	if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf(">>ERROR on initing mutex init!\n");
+        exit(0);
+    }
+
+	/* First call to socket() function to create */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		printf(">>ERROR on opening socket!\n");
+		exit(0);
+	}
+
+	/* Enable address reuse */
+	int on = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	
+	/* Initialize socket structure (assign IP, PORT) */
+	bzero((char *) &servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(PORT);
+
+	/* Now bind the host address using bind() call.*/
+	if (bind(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
+		perror(">>ERROR on binding socket!\n");
+		exit(0);
+	}
+      
+	/* Now start listening for the clients, here
+	  + process will go in sleep mode and will 
+	  + wait for the incoming connection
+	*/
+	/* The backlog argument in "listen" specifies the maximum number of queued connections 
+	  + and should be at least 0; the maximum value is system-dependent (usually 5), 
+	  + the minimum value is forced to 0.
+	  Ref: https://stackoverflow.com/questions/36594400/what-is-backlog-in-tcp-connections
+	*/
+	listen(sockfd, 5);
+	clilen = sizeof(cliaddr);
+	
+	/* Accept the quantity of client */
+	while (countclient!=0) {
+		int valofsock = accept(sockfd, (SA*)&cliaddr, &clilen);
+		if (valofsock < 0) {
+			printf(">>ERROR on accepting from server!\n");
+			continue;
+		}
+		newsockfd.push_back(valofsock);
+		countclient--;
+	}
+
+	/* Run all new socket by creating thread */
+	int i=0;
+	while (1)
+	{
+		for (; i<newsockfd.size(); ++i) {
+			pthread_t threadid;
+			if (pthread_create(&threadid, NULL, run, &newsockfd[i]) < 0) {
+				printf(">>ERROR on creating thread from server!\n");
+				exit(0);
+			}		
+			pthread_detach(threadid);
+		}
+	}
+
+	pthread_mutex_destroy(&lock);
+	return 0;
+} 
+
