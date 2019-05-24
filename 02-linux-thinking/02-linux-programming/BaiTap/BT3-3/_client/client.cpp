c74dcec393a060071c1cf71e352e5f86d3a092d4
1653072@student.hcmus.edu.vn <1653072@student.hcmus.edu.vn>
2019-05-24 23:33:00 +0700

    update

---
+++ 02-linux-thinking/02-linux-programming/BaiTap/BT3-3/_client/client.cpp
@@ -0,0 +1,286 @@
+﻿#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <vector>
+#include <bits/stdc++.h>  //Lib used for sorting vector in C++
+#include <netdb.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h> 
+#include <sys/types.h> 
+
+#define PORT 8080
+#define SA struct sockaddr
+
+//====================================================================
+//GLOBAL VARIABLES
+vector<int> marbleArray;
+string playername;
+char filename[256];
+
+
+//====================================================================
+//DECLARE FUNCTIONS
+bool isValidPlayerName();
+void inputPlayerName();
+long sumValueOfMarbleArray();
+void writeFile();
+void requestMarble(int sock);
+void sendFileToServer(int sock);
+void receiveRankingFromServer(int sock);
+void run(int sock);
+
+
+//====================================================================
+//IMPLEMENT FUNCTIONS
+bool isValidPlayerName() {
+	if (playername.length() == 0) return false;
+
+	if (playername.length() > 250) {
+		printf("[UNVALID NAME] The length of name can not be bigger than 250!\n");
+		return false;
+	}
+
+	if (playername.compare("RequestMarble") == 0 ) {
+		printf("[UNVALID NAME] Your name can not be `RequestMarble`!\n");
+		return false;
+	}
+
+	if (playername.compare("FileNotFound") == 0 ) {
+		printf("[UNVALID NAME] Your name can not be `FileNotFound`!\n");
+		return false;
+	}
+
+	return true;
+}
+
+void inputPlayerName() {
+	do {
+		/* Input name */
+		printf("[NOTICE] Input your player name: ");
+		fflush(stdin);
+		getline(cin, playername);
+	}
+	while (!isValidPlayerName());
+	
+	/* Remove all space */
+	playername.erase(remove_if(playername.begin(), playername.end(), ::isspace), playername.end());
+
+	/* Add '\0' to end of playername string */
+	playername += '\0';
+
+	/* Name after removing space */
+	printf("[NOTICE] Your player name removed space: ");
+	printf("%s\n", playername.c_str());
+
+	/* Update filename for storing data.
+	   Convert string to char[256] */
+	strcpy(filename, (playername + ".txt\0").c_str());
+}
+
+long sumValueOfMarbleArray() {
+	long sum = 0;
+	for (int i = 0; i < marbleArray.size(); ++i) {
+		sum += marbleArray[i];
+	}
+	return sum;
+}
+
+void writeFile() {
+	/* Check size of marbleArray */
+	if (marbleArray.size() == 0) return;
+
+	/* Store data of marbleArray to file */
+	FILE *fp;
+	fp = fopen(filename, "w+");
+	for (int i = 0; i < marbleArray.size(); ++i) {
+		if (i != (marbleArray.size() - 1)) fprintf(fp, "%d\n", marbleArray[i]);
+		else fprintf(fp, "%d", marbleArray[i]);
+	}
+	fclose(fp);
+}
+
+void requestMarble(int sock) {
+	while (1) {
+		/* Send command "RequestMarble" to server */
+		write(sock, "RequestMarble", 13);
+
+		/* Receive value of marble from server.
+		   If server runs out of marbles. Value will be -1 */
+		int val;
+		read(sock, &val, sizeof(val));
+		val = ntohl(val);
+		if (val == -1) break;
+		else {
+			/* Push value received from server */
+			marbleArray.push_back(val);
+			/* Sort ascending marbleArray */
+			sort(marbleArray.begin(), marbleArray.end());
+			/* Store data to file */
+			writeFile();
+		}
+	}
+
+	printf("[NOTICE] Server ran out of marbles!\n");
+	printf("[NOTICE] Writing marbles information to file completely!\n");
+	printf("[NOTICE] You got your marbles | Total marbles: %lu | Total score: %ld\n", marbleArray.size(), sumValueOfMarbleArray());
+
+	//Comparison between qsort (C) and sort (C++)
+	//https://www.geeksforgeeks.org/c-qsort-vs-c-sort/
+}
+
+void sendFileToServer(int sock) {
+	FILE *fp;
+	fp = fopen(filename, "r");
+
+	/* Check file exists or not. If not,
+	   send command "FileNotFound" to server & 
+	   stop sending file from client to server */
+	if (!fp)
+	{
+		while (1) {
+			int check = write(sock, "FileNotFound", 12);
+			if (check > 0) break;
+		}
+		printf("[NOTICE] File not found or you don't have any marbles for SENDING to server!\n");
+		fclose(fp);
+		return;
+	}
+
+	/* Send player name to server */
+	char tmp[256];
+	bzero(tmp, 256);
+	strcpy(tmp, playername.c_str());
+	while (1) {
+		int check =	write(sock, tmp, strlen(tmp));
+		if (check > -1 ) break;
+	}
+
+	/* Wait until receive command "ServerReceivedPlayerName" from server */
+	bzero(tmp, 256);
+	while (1) {
+		read(sock, tmp, sizeof(tmp)-1);
+		if (strcmp(tmp, "ServerReceivedPlayerName") == 0) break;
+	}
+
+	/* Send data of file to server */
+	fp = fopen(filename, "r");
+	int nread = -1;
+	while (1) {
+		/* Receive command "RequestDataOfFile" from server */
+		bzero(tmp, 256);
+		read(sock, tmp, 255);
+		if (strcmp(tmp, "RequestDataOfFile") == 0)
+		{	
+			/* Send -1 to server => End file */
+			if (nread>0 && nread<1024) 
+			{
+				write(sock, "-1", 2);
+				break;
+			}
+			/* Send data to server */
+			else 
+			{
+				unsigned char buff[1024];
+				bzero(buff, 1024);
+				nread = fread(buff, 1, 1024, fp);
+				if (nread > 0) write(sock, buff, nread);
+			}
+		}
+	}
+	fclose(fp);
+
+	printf("[NOTICE] Sending file of marbles to server successfully!\n");
+}
+
+void receiveRankingFromServer(int sock) {
+	/* Receive command "SendRankingFromServer" from server*/
+	char tmp[256];
+	bzero(tmp, 256);
+	while (1) {
+		read(sock, tmp, sizeof(tmp)-1);
+		if (strcmp(tmp, "SendRankingFromServer") == 0) break;
+	}
+
+	/* Receive file ranking from server */
+	char rankingfile[256];
+	bzero(rankingfile, 256);
+	strcpy(rankingfile, ("ranking_" + playername + ".txt").c_str());
+
+	FILE *fp;
+	fp = fopen(rankingfile, "w+");
+
+	int bytesReceived = 0;
+	char buff[1024];
+	while ((bytesReceived = read(sock, buff, 1024)) > 0) {
+		fwrite(buff, 1, bytesReceived, fp);
+		bzero(buff, 1024);
+	}
+
+	fclose(fp);
+
+	/* Open file again & print result */
+	FILE *f;
+	f = fopen(rankingfile, "r");
+	
+	printf("\nSTT\t Name\t Score\n");
+	while (!feof(f)) {
+		bzero(tmp, 256);
+		fgets(tmp, 256, f);	
+		printf("%s\n", tmp);
+	}
+
+	fclose(f);
+}
+
+void run(int sock) {
+	/* Request marble to server and Receive marble from server.
+	   After that, sort ascending marbleArray and write to file.*/
+	requestMarble(sock);
+	/* Send file to server */
+	sendFileToServer(sock);
+	/* Receive ranking from server */
+	receiveRankingFromServer(sock);
+}
+
+
+//====================================================================
+//MAIN FUNCTIONS
+int main() {
+    int sockfd;
+    struct sockaddr_in servaddr; 
+
+	/* First call to socket() function to create */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		printf("[ERROR] Opening socket fail!\n");
+		exit(0);
+	}
+
+	/* Initialize socket structure (assign IP, PORT) */
+	bzero((char *)&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(PORT);
+
+	/* Input player name before starting the game */
+	inputPlayerName();
+
+	/* Now connect the client socket to the server socket */
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
+		printf("[ERROR] Connecting to server fail!\n");
+		exit(0);
+	}
+	else printf("[NOTICE] Connecting to server successfully. Please wait until enough clients join the game...\n");
+
+	/* Run program */
+	run(sockfd);
+    
+	/* Close socket */
+    close(sockfd); 
+	return 0;
+} 
+
--- 02-linux-thinking/02-linux-programming/BaiTap/BT3-3/_server/server.cpp
+++ 02-linux-thinking/02-linux-programming/BaiTap/BT3-3/_server/server.cpp
@@ -16,16 +16,33 @@ using namespace std;
 #define SA struct sockaddr 
 
 //====================================================================
+//STRUCTS
+struct player {
+	char playername[256];
+	long result;
+};
+
+
+//====================================================================
 //GLOBAL VARIABLES
 vector<int> marbleArray;
 pthread_mutex_t lock;
+int countclient = 0;
+vector<player> players;
+bool finishRanking = false;
 
 
 //====================================================================
 //DECLARE FUNCTIONS
 vector<int> randomArray();
-void sendMarble(int sock);
+long sumValueOfArray(vector<int> arr);
+void sendValueOfMarble(int sock);
+void receiveFileFromClient(int sock);
 void *run(void *argsock);
+void sortRanking();
+void sendRankingFileToClient(vector<int> newsockfd);
+void closeAllSocket(vector<int> newsockfd);
+void *finish(void *argsock);
 
 
 //====================================================================
@@ -39,17 +56,25 @@ vector<int> randomArray() {
 
 	//The value received from randomization belongs to [1, 1000]
 	for (int i=0; i<arrsize; ++i) {
-		newarr.push_back(rand() % 1000) + 1; 
+		newarr.push_back(rand() % 1000 + 1); 
 	}
 
 	return newarr;
 }
 
+long sumValueOfArray(vector<int> arr) {
+	long sum = 0;
+	for (int i = 0; i < arr.size(); ++i) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
 void sendValueOfMarble(int sock) {
 	char buffer[256];
-	bool stop = false;
+	int val = -2;
 
-	while (!stop) {
+	while (val != -1) {
 		/* Receive command "RequestMarble" from client */
 		bzero(buffer, 256);
 		read(sock, buffer, 255);
@@ -59,20 +84,79 @@ void sendValueOfMarble(int sock) {
 			pthread_mutex_lock(&lock);
 			if (marbleArray.size() != 0)
 			{
-				int val = htonl(marbleArray[0]);
+				val = htonl(marbleArray[0]);
 				marbleArray.erase(marbleArray.begin());
 				write(sock, &val, sizeof(val));
 			}
 			/* Send -1 (server runs out of marbles) and Quit sending marbles */
 			else
 			{
-				int val = htonl(-1);
+				val = htonl(-1);
 				write(sock, &val, sizeof(val));
-				stop = true;
 			}
 			pthread_mutex_unlock(&lock);
 		}
 	}
+}
+
+void receiveFileFromClient(int sock) {
+	/* Receive player name from client or command "FileNotFound" */
+	char playername[256];
+	bzero(playername, 256);
+	while (read(sock, playername, sizeof(playername)-1) > 0) {
+		if (strcmp(playername, "FileNotFound") == 0) return;
+		write(sock, "ServerReceivedPlayerName", 24);
+		break;
+	}
+	
+	/* If clients send player name to server successfully,
+		+ that means they have marbles and file.
+		+ Write content (by bytes) to file */
+	char filename[256];
+	bzero(filename, 256);
+	strcpy(filename, playername);
+	strcat(filename, ".txt\0");
+
+	FILE *fp;
+	fp = fopen(filename, "w+");
+
+	int bytesReceived = 0;
+	char buff[1024];
+	while (1) {
+		/* Send command "RequestDataOfFile" to client */
+		write(sock, "RequestDataOfFile", 17);
+
+		/* Receive data of file from client */
+		bytesReceived = read(sock, buff, 1024);
+		if (strcmp(buff, "-1") == 0) break; //END OF FILE => break
+		fwrite(buff, 1, bytesReceived, fp);
+		bzero(buff, 1024);
+	}
+
+	fclose(fp);
+
+	/* Open file again & load value of marbles */
+	FILE *f;
+	f = fopen(filename, "r");
+
+	vector<int> marbleOfClient;
+	int val;
+	while (!feof(f)) {
+		fscanf(f, "%d", &val);		
+		marbleOfClient.push_back(val);
+	}
+
+	fclose(f);
+
+	/* Sum value of marbleOfClient */
+	long sum = sumValueOfArray(marbleOfClient);
+
+	/* Server store result of client */
+	player var;
+	strcpy(var.playername, playername);
+	var.result = sum;
+
+	players.push_back(var);
 }
 
 void *run(void *argsock) {
@@ -82,26 +166,84 @@ void *run(void *argsock) {
 	/* Send marble to client when they request */
 	sendValueOfMarble(sock);
 
-	//GetNameOfFile from Client
-	//Compute the result
+	/* Receive file from client */
+	receiveFileFromClient(sock);
 
-	/* Close socket & thread */
-    close(sock);
-    pthread_exit(NULL); 
+	/* Close thread */
+	pthread_exit(NULL); 
+}
+
+void sortRanking() {
+	/* Sort ranking */
+	for (int i=0; i<players.size()-1; ++i) {
+		for (int j=i+1; j<players.size(); ++j) {
+			if (players[i].result < players[j].result)
+				swap(players[i], players[j]);
+		}
+	}
+
+	/* Store data to file "ranking.txt" */
+	FILE *fp;
+	fp = fopen("ranking.txt", "w+");
+
+	for (int i = 0; i < players.size(); ++i) {
+		if (i != (players.size() - 1)) 
+		{
+			fprintf(fp, "%d.\t %s \t %ld\n", i+1, players[i].playername, players[i].result);
+		}
+		else
+		{
+			fprintf(fp, "%d.\t %s \t %ld", i+1, players[i].playername, players[i].result);
+		}
+	}
+
+	fclose(fp);
+}
+
+void sendRankingFileToClient(vector<int> newsockfd) {
+	for (int i=0; i< newsockfd.size(); ++i) {
+			/* Send command "SendRankingFromServer" to client */
+			while (1) {
+				int check = write(newsockfd[i], "SendRankingFromServer", 21);
+				if (check > 0) break;
+			}
+
+			/* Send data of "ranking.txt" to client */
+			FILE *fp;
+			fp = fopen("ranking.txt", "r");
+			while (1) {
+				unsigned char buff[1024];
+				bzero(buff, 1024);
+				int nread = fread(buff, 1, 1024, fp);
+				if (nread > 0) write(newsockfd[i], buff, nread);
+				if (nread < 1024) break;
+			}
+			fclose(fp);
+
+			usleep(2000);
+	}
+}
+
+void closeAllSocket(vector<int> newsockfd) {
+	for (int i=0; i<newsockfd.size(); ++i) {
+		close(newsockfd[i]);
+	}
 }
 
 
 //====================================================================
 //MAIN FUNCTIONS
 int main() {
-    int sockfd;
-	int countclient = 0;
+  int sockfd;
 	vector<int> newsockfd;
 	socklen_t clilen;
-    struct sockaddr_in servaddr, cliaddr;  
+  struct sockaddr_in servaddr, cliaddr;  
 
 	/* Randomizing value of array for marbleArray */
 	marbleArray = randomArray();
+
+	//test
+	cout<<marbleArray.size()<<endl<<sumValueOfArray(marbleArray)<<endl;
 
 	/* Input the quantity of client */
 	while (countclient<3 || countclient>9) {
@@ -152,14 +294,16 @@ int main() {
 	clilen = sizeof(cliaddr);
 	
 	/* Accept the quantity of client */
-	while (countclient!=0) {
+	int counttemp = countclient;
+	printf("[NOTICE] Waiting %d clients connecting...\n", counttemp);
+	while (counttemp!=0) {
 		int valofsock = accept(sockfd, (SA*)&cliaddr, &clilen);
 		if (valofsock < 0) {
-			printf("[ERROR] Accepting from server fail!\n");
+			printf("[ERROR] Accepting client fail!\n");
 			continue;
 		}
 		newsockfd.push_back(valofsock);
-		countclient--;
+		counttemp--;
 	}
 
 	/* Run all new sockets by creating thread. After creating thread based on value of newsockfd array,
@@ -167,6 +311,7 @@ int main() {
 	int i=0;
 	while (1)
 	{
+		if (players.size() == countclient) break;
 		for (; i<newsockfd.size(); ++i) {
 			pthread_t threadid;
 			if (pthread_create(&threadid, NULL, run, &newsockfd[i]) < 0) {
@@ -176,6 +321,10 @@ int main() {
 			pthread_detach(threadid);
 		}
 	}
+
+	sortRanking();
+	sendRankingFileToClient(newsockfd);
+	closeAllSocket(newsockfd);
 
 	pthread_mutex_destroy(&lock);
 	return 0;
